3e38264b8853bf97d33afbc94977316f14b72df2
nranmag <43456295+nranmag@users.noreply.github.com>
2019-08-03 14:17:08 -0500

    Java libreries
    Libreries for Projects Java

---
+++ Librerías/Jconexion-master/README.md
@@ -0,0 +1,30 @@
+# Jconexion
+Framework for manage the data base and other common code in projects of java.
+
+<h1>1Steps</h1>
+<h3>1</h3>
+Copy and paste src into your project. <br>
+
+<h3>2</h3>
+Add the folder lib to Libraries of your project.<br>
+
+<h3>3</h3>
+Go to the File Options -> System.Settings.Options.java and set the configuration.<br>
+<br>
+Line 23: modeDevelop = Terminal or GUI;<br>
+Line 50: showSQL = true;<br>
+Line 60: driver = MySQ, SQLSRV or Postgre.<br>
+<br>
+MySQL   => MySQL<br>
+SQLSRV  => SQL Server<br>
+Postgre => PostgreSQL<br>
+
+<h3>4</h3>
+Run app
+<br>
+Configure the DB<br>
+MySQL   => DataBase;User;Password;<br>
+SQLSRV  => NamePCServer;DataBase*User*Password;<br>
+Postgre => DataBase;User;Password;<br>
+<br>
+Check the video on: [Upload :D lol]
---
+++ Librerías/Jconexion-master/src/Aplicacion/Vistas/Index.java
@@ -0,0 +1,92 @@
+
+package Aplicacion.Vistas;
+
+import Aplicacion.Controller.IndexController;
+import System.MVC.Core.IView;
+import System.MVC.Core.View;
+import javax.swing.JComponent;
+import javax.swing.JOptionPane;
+
+/**
+ * Frame secundario del API, pero principal del sistema que se piensa desarrollar
+ * @author Nekszer
+ * @version 1.3.0
+ * @since JConexionDB 1.0
+ */
+public class Index extends View implements IView{
+    
+    public Index() {
+        initComponents();
+        setVisible(true);
+        setTitle("Ventana principal del sistema");
+        setLocationRelativeTo(null);
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel2 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Welcome to JConexion");
+
+        jButton1.setText("Salir de la aplicacion");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 798, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(661, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(31, 31, 31)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 420, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        onEventFromUI(IndexController.btnExitApp, this, (JComponent) null);
+    }//GEN-LAST:event_jButton1ActionPerformed
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel2;
+    // End of variables declaration//GEN-END:variables
+
+    @Override
+    public void onDataReceiver(int action, Object object) {
+        switch (action) {
+            // recibimos la informacion desde el controllador
+            case IndexController.btnExitApp:
+                String data = (String) object; // parceamos al objeto original
+                JOptionPane.showMessageDialog(this, data); // imprimimos la informacion 
+                break;
+            default:
+                System.out.println("No hay un caso para actionid");
+        }
+    }
+    
+}
+
+
